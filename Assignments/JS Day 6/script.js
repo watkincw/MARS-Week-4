96556fe150a9da4804dd36944897072f5db5c57b
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2022-03-07 16:14:33 -0600

    adds the current date as the furthest date in the furture your allowed to choose

--- Assignments/JS Day 6/script.js
+++ Assignments/JS Day 6/script.js
@@ -1,3 +1,35 @@
+// give input type='date' a max value
+function maxDate() {
+    // get current date
+    let today = new Date();
+    // get numbered day of the month from 'today'
+    let dd = today.getDate();
+    // get month from 'today'
+    let mm = today.getMonth() + 1; 
+    // get year from 'today'
+    let yyyy = today.getFullYear();
+
+    // if the dat of the month is less than 10, we need to add a '0' in front to comply with what the 'max' property requires for the date input
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    console.log(dd);
+    // if the month is less than 10, we need to add a '0' in front to conform with what the 'max' property requires for the date input
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    console.log(mm);
+    console.log(yyyy);
+
+    // concatenate 'yyyy' + '-' + 'mm' + '-' + 'dd'
+    valueOfMaxAttribute = yyyy + '-' + mm + '-' + dd;
+
+    // target date input element
+    let dateEl = document.getElementById('hireDate');
+    // give dateEl, aka the date input element, the 'max' attribute which, when coupled with the 'date' attribute, dictates the furthest date in the future your allowed to select(can also be set to a date in the passed)
+    dateEl.setAttribute('max', `${valueOfMaxAttribute}`);
+};
+
 function formData() {
     let name = document.getElementById('employeeName').value;
     let employeeID = Number(document.getElementById('empId').value);
@@ -92,3 +124,6 @@ function formData() {
     sessionStorage.setItem('Employee Name', userData.name);
     sessionStorage.setItem('Wage', wage);
 };
+
+// call maxDate to give the date input a max attribute and assign it todays date
+maxDate();
